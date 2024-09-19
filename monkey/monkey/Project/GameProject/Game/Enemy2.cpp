5717ee62cf87f65e9b88f5807f57adb0c474d374
sogotoya <24051904@d.anabuki-college.net>
2024-09-19 14:41:13 +0900

    toyaikemen

--- monkey/monkey/Project/GameProject/Game/Enemy2.cpp
+++ monkey/monkey/Project/GameProject/Game/Enemy2.cpp
@@ -3,9 +3,17 @@
 Enemy::Enemy(const CVector2D& pos, bool flip)
 	:Base(eType_Enemy)
 {
-	//m_img = COPY_RESOURCE("", CImage);
+	m_img = COPY_RESOURCE("Enemy2", CImage);
 	//再生アニメーション設定
 	m_img.ChangeAnimation(0);
+	//座標
+	m_pos_old = m_pos = pos;
+	//画像表示サイズ 
+	m_img.SetSize(100, 100);
+	//中心位置
+	m_img.SetCenter(100 / 2, 100 / 2);
+	//矩形
+	m_rect = CRect(-25, -100, 25, 0);
 }
 
 void Enemy::Update()
--- monkey/monkey/Project/GameProject/Game/Enemy2.h
+++ monkey/monkey/Project/GameProject/Game/Enemy2.h
@@ -3,9 +3,30 @@
 
 class Enemy : public Base {
 private:
-	//状態
+	enum {
+		eState_Idle,
+		eState_Attack,
+		eState_Damage,
+		eState_Down,
+	};
+	//状態変数
+	int m_state;
 	CImage m_img;
 	bool m_flip;
+	//体力
+	int m_hp;
+	//各状態での挙動
+	void StateIdle();
+	void StateAttack();
+	void StateDamage();
+	void StateDown();
+	//着地フラグ
+	bool m_is_ground;
+	//攻撃番号
+	int m_attack_no;
+
+
+
 public:
 	Enemy(const CVector2D& pos, bool flip);
 	void Update();
