27fb17d3aa311e8080abe56eb2bddee65a1bb9a3
sogotoya <24051904@d.anabuki-college.net>
2024-09-27 12:18:08 +0900

    map範囲作成

--- monkey/monkey/Project/GameProject/Game/Map.h
+++ monkey/monkey/Project/GameProject/Game/Map.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "../Base/Base.h"
 //横
-#define MAP_WIDTH  20
+#define MAP_WIDTH  40
 //縦
-#define MAP_HEIGHT 20
+#define MAP_HEIGHT 22
 //大きさ
 #define MAP_TIP_SIZE 20
 
@@ -13,4 +13,10 @@ private:
 public:
 	Map();
 	void Draw();
+	//指定座標のチップ番号取得
+	int GetTip(const CVector2D& pos);
+	//指定列、指定行のマップチップを取得
+	int GetTip(int col, int row);
+	//マップとの当たり判定
+	int CollisionPoint(const CVector2D& pos);
 };
--- monkey/monkey/Project/GameProject/main.cpp
+++ monkey/monkey/Project/GameProject/main.cpp
@@ -117,7 +117,7 @@ void Init(void)
 	//è\âÕ
 	ADD_RESOURCE("Enemy2", CImage::CreateImage("Image/gobuta.png", enemy2_anim_data, 150, 150));
 	Base::Add(new Enemy2(CVector2D(100, 100), true));
-	ADD_RESOURCE("Map_Tip", CImage::CreateImage("Image/world_tileset.png"));
+	ADD_RESOURCE("Map_Tip", CImage::CreateImage("Image/kusa.png"));
 
 	
 
