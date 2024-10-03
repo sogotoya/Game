e32309ed9192a0ce65286d0281f1f88ecf8a2193
Soma <24051905@d.anabuki-college.net>
2024-10-03 12:26:29 +0900

    h

--- monkey/monkey/Project/GameProject/Game/Map.h
+++ monkey/monkey/Project/GameProject/Game/Map.h
@@ -1,25 +1,27 @@
 #pragma once
 #include "../Base/Base.h"
 //横
-#define MAP_WIDTH  400  //250   
+#define MAP_WIDTH  150  //250   
 //縦
-#define MAP_HEIGHT 400  //250
+#define MAP_HEIGHT 150  //250
 //大きさ
-#define MAP_TIP_SIZE  26   //128
+#define MAP_TIP_SIZE  100  //128
 
 class Map : public Base {
 private:
+	CImage m_background;
 	CImage m_img;
 	int m_state;
 public:
 	Map();
 	void Draw();
 	//指定座標のチップ番号取得
-	int GetTip(const CVector2D& pos);
+	int GetTip(const CVector2D& pos, int* tx = nullptr, int* ty = nullptr);
 	//指定列、指定行のマップチップを取得
-	int GetTip(int col, int row);
+	int GetTip(int x, int y);
 	//マップとの当たり判定
 	int CollisionPoint(const CVector2D& pos);
+	int stage[MAP_HEIGHT][MAP_WIDTH];
 };
 
 
