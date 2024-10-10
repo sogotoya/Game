540b27086c261cacba0c40308a1f4fc55bb380d6
camuchasu <24051908@d.anabuki-college.net>
2024-10-10 15:16:20 +0900

    1

--- monkey/monkey/Project/GameProject/Game/Player.cpp
+++ monkey/monkey/Project/GameProject/Game/Player.cpp
@@ -243,28 +243,28 @@ void Player::Collision(Base* b)
 				m_is_ground = true;
 			}
 		}
-		/*case
-			//Field型へキャスト、型変換できたら
-			if (Map* f = dynamic_cast<Map*>(b)) {
-				//地面より下にいったら
-				if (m_pos.y > f->GetGroundY()) {
-					//地面の高さに戻す
-					m_pos.y = f->GetGroundY();
-					//落下速度リセット
-					m_vec.y = 0;
-					//接地フラグON
-					m_is_ground = true;
+	/*case
+		//Field型へキャスト、型変換できたら
+		if (Map* f = dynamic_cast<Map*>(b)) {
+			//地面より下にいったら
+			if (m_pos.y > f->GetGroundY()) {
+				//地面の高さに戻す
+				m_pos.y = f->GetGroundY();
+				//落下速度リセット
+				m_vec.y = 0;
+				//接地フラグON
+				m_is_ground = true;
 
-				}
 			}
-			break;*/
+		}
+		break;*/
 	case eType_bar:
 		if (Base::CollisionRect(this, b)) {
 			if (m_pos_old.y + m_rect.m_bottom <= b->m_pos_old.y + b->m_rect.m_top) {
 				m_pos.y = b->m_pos.y + b->m_rect.m_top;
 				m_is_ground = true;//着地
 				m_vec.y = 0;
-
+				
 			}
 
 
@@ -291,6 +291,8 @@ void Player::Collision(Base* b)
 		}
 		break;
 	}
+
+
 }
 
 static TexAnim playerIdle[] = {
@@ -355,7 +357,7 @@ static TexAnim playerJumpup[] = {
 	{ 18,3 },
 };
 static TexAnim playerJumpDown[] = {
-	{19, 3 },
+	{ 19,3 },
     { 20,3 },
     { 21,3 },
     { 22,3 },
